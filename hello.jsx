18883e641110e3011a5704dbc607d4035aa88f63
kanade <kanade147359@keio.jp>
2023-01-11 18:58:55 +0900

    modified hello.jsx

--- hello.jsx
+++ hello.jsx
@@ -1,2 +1,4 @@
 app.newProject().items.addComp("myComp", 1920, 1080, 1.0, 15, 29.97).layers.addText("Hello World");
-    
+
+var comp = app.project.items.addComp("myConp", 1920,1080, 1.0,15, 29.97);
+comp.layers.addSolid([1, 0, 0], "red flat", 960, 540, 1.0, 15);
